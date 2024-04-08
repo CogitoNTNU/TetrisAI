92255a941c19557870b6ae5be8453c743a39ab99
Jon Bergland <jobergl@stud.ntnu.no>
2024-04-08 20:04:13 +0200

    feat: add method to get possible moves for the board

--- src/game/board.py
+++ src/game/board.py
@@ -1,7 +1,10 @@
+from typing import Callable
 import pygame
 import random
 import copy
-from block import Block
+from enum import Enum, auto
+
+from src.game.block import Block
 
 '''
 Denne skriver ut brettet i terminal bare. 
@@ -10,6 +13,18 @@ Dersom en hel rad i matrisen er full, så fjerner vi denne raden og flytter alt 
 En brikke i matrisen represneteres med 1-er. 
 '''
 
+
+
+class Action(Enum):
+    MOVE_LEFT = auto()
+    MOVE_RIGHT = auto()
+    ROTATE_CLOCKWISE = auto()
+    ROTATE_COUNTERCLOCKWISE = auto()
+    DROP = auto()
+    SOFT_DROP = auto()
+    HOLD = auto()
+
+
 class Board:
     rows = 20
     columns = 10
@@ -17,6 +32,7 @@ class Board:
     rowsRemoved = 0
     board=[]
     block= None
+    nextBlock = None
     # block = Block(0,5, random.randint(0,6)) #random.randint(0,6) er for å velge en tilfeldig brikke
 
     def __init__(self):
@@ -32,12 +48,20 @@ class Board:
         # self.block = Block(3,0, random.randint(0,6))
         self.placeBlock()
 
+        self.nextBlock = Block(0,5, random.randint(0,6))
+
+    def getBoard(self):
+        return copy.deepcopy(self.board)
+
     def printBoard(self):
         print("___________________________________")
         for row in self.board:
             print('|' + '   '.join(self.checkCharacter(cell) for cell in row) + '|')
         
         print("‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾")
+
+    def setBlock(self, block: Block) -> None:
+        self.nextBlock = block
 
     def checkCharacter(self, character):
         if character == 1:
@@ -56,15 +80,13 @@ class Board:
     def moveBlockDown(self):
         if self.validMove(self.block.moveDown):
             self.placeBlock()
-     
 
-    
+
     def moveBlockLeft(self):
         if self.validMove(self.block.moveLeft):
             self.placeBlock()
-      
-        
-            
+
+
     def moveBlockRight(self): 
         if self.validMove(self.block.moveRight):
             self.placeBlock()
@@ -75,36 +97,20 @@ class Board:
         if self.validMove(self.block.rotateLeft):
             self.placeBlock()
           
-                
-    def gameOver(self):
+
+    def isGameOver(self):
         return self.gameOver
+
 
     def setGameOver(self,state):
         self.gameOver = state
 
-    
-    
 
-    
     def blockLanded(self):
-        
         pass
 
-   
 
-
-
-    # def validMove(self):
-    #     for i in range(4):
-    #         for j in range(4):
-    #             if i * 4 + j in self.block.image():
-    #                 if i + self.block.y > self.rows - 1 or \
-    #                         j + self.block.x > self.columns - 1 or \
-    #                         j + self.block.x < 0 or \
-    #                         self.board[i + self.block.y][j + self.block.x] > 0:
-    #                     return False  # Returnerer False ved kollisjon eller ugyldig trekk
-
-    def validMove(self, simulateimulatedMove):
+    def validMove(self, simulateimulatedMove: Callable):
         # if simulated move fails = move out of bounds and should be disallowed
         simulateimulatedMove()
         
@@ -146,6 +152,7 @@ class Board:
 
     
     def placeBlock(self):
+        """ Places the current block on the board """
         self.board = copy.deepcopy(self.prevBoard)
         # if self.validMove():
         for i in range(4):
@@ -162,12 +169,63 @@ class Board:
                     self.board[i + self.block.y][j + self.block.x] = 1 #self.block.color
         return self.checkGameState() #hvis denne sjekkes hver gang og gir false, var det ikke mulig å plassere blokken og spillet er over ffs.
 
+
+    def getPossibleMoves(self) -> list["Board"]:
+        possibleMoves = []
+        firstRotation = self.block.rotation
+        currentRotation = firstRotation
+        
+        while(True):
+            for i in range(self.columns):
+                moveBoard = copy.deepcopy(self)
+                moveBoard.block.setCoordinates(i, 0)
+                for j in range(currentRotation):
+                    moveBoard.block.rotateRight()
+                moveBoard.placeBlock()
+                
+                while moveBoard.validMove(moveBoard.block.moveDown):
+                    moveBoard.block.moveDown()
+                
+                possibleMoves.append(copy.deepcopy(moveBoard))
+
+            moveBoard.block.rotateRight()
+            currentRotation += 1
+
+            if(moveBoard.block.rotation != firstRotation):
+                continue
+            else:
+                break
+
+
+
+def transition_model(current_state: Board, target_state: Board) -> list[Action]:
+    """
+    Returns the sequence of actions needed to transition from the current state to the target state.
+
+    Args:
+        new_state: The new state to transition to
+
+    Returns:
+        The resulting state
+    """
+    
+    actions = []
+
+    if current_state == target_state:
+        return actions
+    
+    # Find where the last block is in the target state
+    target_block = target_state.block
+    
+    # Find the correct rotation
+    needed_rotation = target_block.rotation - current_state.block.rotation
+    actions += [Action.ROTATE_CLOCKWISE] * needed_rotation
     
 
+    # Move the block to the correct x and y position
+    if current_state.block.x < target_block.x:
+        actions += [Action.MOVE_RIGHT] * (target_block.x - current_state.block.x)
+    elif current_state.block.x > target_block.x:
+        actions += [Action.MOVE_LEFT] * (current_state.block.x - target_block.x)
 
-    
-
-    
-
-
-    
+    return actions
