3ebf6cd8fade77d766d78bdd6f2552fb8a1d005a
Jon Bergland <jobergl@stud.ntnu.no>
2024-04-08 18:59:07 +0200

    feat: Add heuristic module with utility and aggregate_heights functions

---
+++ src/agents/heuristic.py
@@ -0,0 +1,12 @@
+""" The heuristic module contains the heuristics used by the agents. """
+
+from src.game.board import Board
+
+def utility(gameState: Board) -> int:
+    """ Returns the utility of the given game state. """
+    pass
+
+
+def aggregate_heights(gameState: Board) -> int:
+    """ Returns the sum of the heights of the columns in the game state. """
+    pass
