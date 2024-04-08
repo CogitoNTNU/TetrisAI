a12fd3674ec9cb8218c8f18ba0b3ebef93765c64
Jon Bergland <jobergl@stud.ntnu.no>
2024-04-08 20:02:17 +0200

    feat: implement result in randomAgent

--- src/agents/randomAgent.py
+++ src/agents/randomAgent.py
@@ -1,7 +1,12 @@
 from src.agents.agent import Agent
 from src.game.board import Board
 
+from random import random
+
 class RandomAgent(Agent):
-    
+    """Random agent that selects a random move from the list of possible moves"""
+
     def result(board: Board) -> Board:
-        pass
+        possible_moves = board.getPossibleMoves()
+        move = random.choice(possible_moves)
+        return board.makeMove(move)
