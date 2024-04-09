24cbd1033bd742ca2b62163ff42c84ff755b6fbe
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2024-04-10 00:06:41 +0200

    feat: change Agent.result to return an action or sequence of actions

--- src/agents/agent.py
+++ src/agents/agent.py
@@ -4,9 +4,10 @@ class for all agents in the simulation.
 """
 
 from abc import ABC, abstractmethod
-from typing import Any
+from typing import Any, Union
 
-from src.game.board import Board
+from src.game.board import Action, Board
+
 
 class Agent(ABC):
     """Base class for all agents in the simulation."""
@@ -14,24 +15,21 @@ class Agent(ABC):
     @classmethod
     def __instancecheck__(cls, instance: Any) -> bool:
         return cls.__subclasscheck__(type(instance))
-    
+
     @classmethod
     def __subclasscheck__(cls, subclass: Any) -> bool:
-        return (
-            hasattr(subclass, 'result') and
-            callable(subclass.result)
-        )   
+        return hasattr(subclass, "result") and callable(subclass.result)
 
     @abstractmethod
-    def result(board: Board) -> Board:
+    def result(board: Board) -> Union[Action, list[Action]]:
         """
         Determines the next move for the agent based on the current state of the board.
 
         Args:
             board (Board): The current state of the board.
-        
+
         Returns:
-            The new state of the board after the agent has made a move.
+            The next move for the agent. This can be a single action or a list of actions.
         """
         pass
 
