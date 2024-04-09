ed37dc665626f1590ecf94092c6f23ad12bffd47
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2024-04-09 04:57:33 +0200

    docs: add docstrings

--- src/game/block.py
+++ src/game/block.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 import random
 
 FIGURES = [
+    # Definitions for each Tetris block rotation
     [[1, 5, 9, 13], [4, 5, 6, 7], [2, 6, 10, 14], [8, 9, 10, 11]],  # I
     [[4, 5, 9, 10], [2, 6, 5, 9], [0, 1, 5, 6], [1, 5, 4, 8]],  # Z
     [[9, 10, 6, 7], [1, 5, 6, 10], [5, 6, 2, 3], [2, 6, 7, 11]],  # S
@@ -13,6 +14,7 @@ FIGURES = [
 
 # Colors for the blocks
 COLORS = [
+    # RGB color definitions for each block type
     (0, 255, 255),  # I
     (255, 0, 0),  # Z
     (0, 255, 0),  # S
@@ -25,8 +27,26 @@ COLORS = [
 
 @dataclass
 class Block:
+    """
+    Represents a Tetris block, encapsulating its position, type, rotation, and color.
+
+    Attributes:
+        x (int): The x-coordinate of the block on the Tetris grid.
+        y (int): The y-coordinate of the block on the Tetris grid.
+        rotation (int): The rotation state of the block, representing its orientation.
+        type (int): The type of the block, determining its shape and color.
+        color (tuple): The RGB color of the block.
+    """
 
     def __init__(self, x: int, y: int, blockType: int = None):
+        """
+        Initializes a Block instance with specified x and y coordinates, and an optional block type.
+
+        Parameters:
+            x (int): The initial x-coordinate of the block.
+            y (int): The initial y-coordinate of the block.
+            blockType (int, optional): The type of the block. Randomly selected if not provided.
+        """
         self.x = x
         self.y = y
         self.rotation = 0
@@ -35,6 +55,12 @@ class Block:
         self.color = COLORS[self.type]
 
     def copy(self) -> "Block":
+        """
+        Creates a copy of the block with the same position, type, and rotation.
+
+        Returns:
+            Block: A new instance of Block with identical properties.
+        """
         block = Block(self.x, self.y, self.type)
         block.rotation = self.rotation
         return block
@@ -65,6 +91,12 @@ class Block:
         return FIGURES[self.type][self.rotation]
 
     def getListCoordinates(self) -> list:
+        """
+        Calculates and returns the grid coordinates for each part of the block, based on its current position and orientation.
+
+        Returns:
+            list of tuple: A list of tuples (x, y) representing the grid coordinates of the block's parts.
+        """
         imageList = self.image()
         listCoordinates = []
         for i in range(len(imageList)):
