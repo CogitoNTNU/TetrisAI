bb4f9c2ab70a7a68af0a1771dc78fc6292f213e2
henrinha <henrinha@stud.ntnu.no>
2024-04-09 19:54:51 +0200

    feat: implement bumpiness, aggregate height, lines cleaned methods

--- src/agents/heuristic.py
+++ src/agents/heuristic.py
@@ -26,3 +26,42 @@ def max_height(gameState: Board) -> int:
                 if checkedList[j] == 0:
                     checkedList[j] = gameState.rows - i
     return max(checkedList)
+
+def lines_cleaned(gameState: Board) -> int:
+    """ Retrurns the number of lines cleared. """
+    sum = 0
+    for row in gameState.board:
+        if all(cell == 1 for cell in row):
+            sum += 1
+    return sum
+
+def bumpiness(gameState: Board) -> int:
+    """ Returns the sum of the absolute height between all the columns"""
+    total_bumpiness = 0
+    max_height = 20
+    columnHeightMap = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
+    for column in range(gameState.columns):
+        for row in range (gameState.rows):
+            if gameState.board[row][column] > 0:
+                if columnHeightMap[column] == 0:
+                    columnHeightMap[column] = max_height - row 
+                
+    for key in range(gameState.columns-1):
+        total_bumpiness += abs(columnHeightMap[key] - columnHeightMap[key+1])
+    return total_bumpiness
+
+def aggregate_height(gameState: Board) -> int:
+    " Returns the sum of all column-heights "
+    max_height = 20
+    total_height = 0
+    columnHeightMap = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
+    for column in range(gameState.columns):
+        for row in range (gameState.rows):
+            if gameState.board[row][column] > 0:
+                if columnHeightMap[column] == 0:
+                    columnHeightMap[column] = max_height - row
+                
+    for key in range(gameState.columns):
+        total_height += columnHeightMap[key]
+    return total_height
+    
