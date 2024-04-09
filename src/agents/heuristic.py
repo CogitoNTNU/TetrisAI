20014835b05d3519af84bc07856546f24fbe988f
Maia Austigard <maustigard@gmail.com>
2024-04-09 18:50:28 +0200

    feat: implement finding hole heuristic

--- src/agents/heuristic.py
+++ src/agents/heuristic.py
@@ -26,3 +26,23 @@ def max_height(gameState: Board) -> int:
                 if checkedList[j] == 0:
                     checkedList[j] = gameState.rows - i
     return max(checkedList)
+
+def find_holes(gameState: Board) -> int:
+    """ Returns number of empty cells on the board. 
+    
+    Args:
+        gameState (Board): the state to check
+
+    Returns: 
+        The heuristic value
+    """
+    holes = 0
+    for i in range(gameState.columns):
+        top_block = 20
+        for j in range(gameState.rows):
+            if (gameState.board[j][i] == 1) and (j < top_block):
+                top_block = j
+            if (gameState.board[j][i] == 0) and (j > top_block):
+                holes += 1
+
+    return holes
