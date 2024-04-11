28dfa2a531d711a81e961053588ba13d66e8d221
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2024-04-11 02:12:32 +0200

    test: add test cases for get_possible_moves_for_square in test_board.py

--- test/game/test_board.py
+++ test/game/test_board.py
@@ -4,16 +4,16 @@ from src.game.board import Action, Board, transition_model
 import copy
 
 
-# def test_get_possible_moves():
-#     board: Board = Board()
-#
-#     board.setBlock(Block(0, 5, 5))
-#     possible_moves = board.getPossibleMoves()
-#     assert isinstance(possible_moves, list)
-#     for move in possible_moves:
-#         assert isinstance(move, Board)
-#
-#     assert len(possible_moves) == 9
+def test_get_possible_moves_for_square():
+    first_block = Block(0, 3, 6)
+    board: Board = Board(block=first_block)
+
+    possible_moves = board.getPossibleBoards()
+    assert isinstance(possible_moves, list)
+    for move in possible_moves:
+        assert isinstance(move, Board)
+
+    assert len(possible_moves) == 8
 
 
 def test_board_equal_for_the_same_object():
