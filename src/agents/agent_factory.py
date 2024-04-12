17e30171ee95a1908baa8f7939e7500f850770e6
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2024-04-13 01:45:03 +0200

    refactor: change script name

--- src/agents/agent_factory.py
+++ src/agents/agent_factory.py
@@ -1,7 +1,9 @@
 """ This module contains the factory function for creating agents. """
 
 from src.agents.agent import Agent
-from src.agents.randomAgent import RandomAgent
+from src.agents.random_agent import RandomAgent
+
+
 
 def create_agent(agent_type: str) -> Agent:
     """ Create an agent of the specified type. """
