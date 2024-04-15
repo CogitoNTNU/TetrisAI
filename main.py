32649d396b42d55e5c3304ef81062c0dbe6f8b0a
Eduard Prokhorikhin <eduardprokhorikhin@gmail.com>
2024-04-15 17:08:17 +0200

    feat: transitioned input managmentm to pygame

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from src.game.tetris import Tetris
+from src.game.TetrisGameManager import *
 from src.agents.agent import Agent, play_game
 from src.agents.agent_factory import create_agent
 
 if __name__ == "__main__":
-    game = Tetris()
-    agent: Agent = create_agent("heuristic")
-    end_board = play_game(agent, game, 7)
-    print(f"There was {end_board.rowsRemoved} rows removed")
+    # game = Tetris()
+    # agent: Agent = create_agent("heuristic")
+    # end_board = play_game(agent, game, 7)
+    # print(f"There was {end_board.rowsRemoved} rows removed")
+    
+    board = Tetris()
+    manager = TetrisGameManager(board)
+    
+    manager.startGame()
