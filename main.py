bd7bc636c721ca1e3dc21cd0a6e68a2cd5bd6ed0
henrinha <henrinha@stud.ntnu.no>
2024-04-15 18:24:53 +0200

    feat: made heuristic agent

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from src.agents.agent import Agent, play_game
 from src.agents.agent_factory import create_agent
 
 if __name__ == "__main__":
-    # game = Tetris()
-    # agent: Agent = create_agent("heuristic")
-    # end_board = play_game(agent, game, 7)
-    # print(f"There was {end_board.rowsRemoved} rows removed")
+    game = Tetris()
+    agent: Agent = create_agent("heuristic")
+    end_board = play_game(agent, game, 7)
+    print(f"There was {end_board.rowsRemoved} rows removed")
     
-    board = Tetris()
-    manager = TetrisGameManager(board)
+    # board = Tetris()
+    # manager = TetrisGameManager(board)
     
-    manager.startGame()
+    # manager.startGame()
--- src/agents/heuristic_agent.py
+++ src/agents/heuristic_agent.py
@@ -1,6 +1,7 @@
 from src.agents.agent import Agent
-from src.game.tetris import Action, Tetris, transition_model
+from src.game.tetris import Action, Tetris, transition_model, get_all_actions
 from src.agents.heuristic import (
+    utility,
     find_holes,
     aggregate_height,
     max_height,
@@ -10,10 +11,23 @@ from src.agents.heuristic import (
 
 class HeuristicAgent(Agent):
 
-    def result(self, board: Tetris) -> Action:
-        # TODO: Get all possible boards
+    def result(self, board: Tetris) -> list[Action]:
+        
+        all_posible_boards = board.getPossibleBoards()
+        best_board: Tetris
+        for possible_board in all_posible_boards:
+            best_score = float("-inf")
+            
+            board_utility = utility(possible_board, 1,1,1,1,1)
+            if board_utility > best_board:
+                best_board = possible_board
+                best_score = board_utility
+                
+
+        
+        # for alle mulige trekk, sjekk heurstikk med utility og velg den beste.
 
         # TODO: Check which board has the best outcome based on the heuristic
 
         # TODO: Find the actions needed to transform the current board to the new board
-        pass
+        return transition_model(board, best_board)
