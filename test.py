9b5f454fecfe8f0e469fef6b7d5f7e252d18fa15
oystkva <oystkva@stud.ntnu.no>
2024-04-22 20:44:45 +0200

    feat: :rocket: genetic agent class and it's training algorithm commenced

---
+++ test.py
@@ -0,0 +1,10 @@
+from src.agents.agent_factory import *
+from src.agents.geneticAlgAgentJon import GeneticAlgAgentJM
+from src.game.TetrisGameManager import *
+from src.game.block import Block
+from src.game.tetris import *
+
+genAgent = GeneticAlgAgentJM()
+
+genAgent.number_of_selection(10)
+
