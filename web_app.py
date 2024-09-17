5bcc5eb68e2340c07828f53c3072adb29f9a2077
Sverre Nystad <sverrenystad@gmail.com>
2024-09-17 16:54:39 +0200

    feat: add highscores post and get endpoints

--- web_app.py
+++ web_app.py
@@ -1,9 +1,16 @@
-from fastapi import FastAPI, WebSocket
+import os
+import json
+from dataclasses import asdict, dataclass
+
+from fastapi import FastAPI, WebSocket, HTTPException
 from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.cors import CORSMiddleware
+
 from src.game.tetris import Tetris
 from src.game.TetrisWebGameManager import TetrisGameManager
 from src.agents.agent_factory import create_agent, AVAILABLE_AGENTS
+
 
 app = FastAPI(
     title="Cogito TetrisAI API",
@@ -26,6 +33,64 @@ async def get_available_agents():
     Returns a list of available agents for playing the game.
     """
     return AVAILABLE_AGENTS
+
+
+@dataclass
+class Score:
+    name: str
+    score: int
+    date: str
+
+
+HIGHSCORES_FILE = "highscores.json"
+
+
+def initialize_file():
+    """Ensure the highscores file exists and contains valid JSON."""
+    if not os.path.isfile(HIGHSCORES_FILE):
+        with open(HIGHSCORES_FILE, "w") as file:
+            json.dump([], file)
+    else:
+        # Check if the file is empty or invalid
+        try:
+            with open(HIGHSCORES_FILE, "r") as file:
+                if not file.read().strip():
+                    # If the file is empty, write an empty list
+                    with open(HIGHSCORES_FILE, "w") as file:
+                        json.dump([], file)
+        except json.JSONDecodeError:
+            # If the file contains invalid JSON, reset it
+            with open(HIGHSCORES_FILE, "w") as file:
+                json.dump([], file)
+
+
+@app.get("/highscores", response_model=list[Score])
+async def get_highscores():
+    initialize_file()
+
+    with open(HIGHSCORES_FILE, "r") as file:
+        scores_data = json.load(file)
+        scores = [Score(**score) for score in scores_data]
+    return scores
+
+
+@app.post("/highscores", response_model=Score)
+async def add_highscore(raw_score: dict):
+    initialize_file()
+
+    # Read existing scores
+    with open(HIGHSCORES_FILE, "r") as file:
+        scores_data = json.load(file)
+
+    # Create new score instance
+    score = Score(**raw_score)
+    scores_data.append(asdict(score))
+
+    # Write the updated scores back to the file
+    with open(HIGHSCORES_FILE, "w") as file:
+        json.dump(scores_data, file)
+
+    return score
 
 
 @app.websocket("/ws/game")
